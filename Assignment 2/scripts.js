647ef975f0660fb2bc329117b9738c9f8ff1eb7a
anshriti <shanshriti1121@gmail.com>
2025-12-28 18:17:55 +0545

    Assignment 2 corrected

--- Assignment 2/scripts.js
+++ Assignment 2/scripts.js
@@ -1,4 +1,4 @@
-/* Document Object Model 
+/* Document Object Model
 Implementation of Ch2 topic 2.1.1, 2.1.2 and 2.1.3 */
 
 // DOM Element Selection
@@ -10,39 +10,59 @@ const btn_output = document.querySelector(".btn_output");
 const transcript = document.getElementById("transcript");
 const show_data = document.getElementById("show_data");
 
-// Counter variable - retrieves from localStorage or initializes to 0
+// Counter variable
 let i = parseInt(localStorage.getItem("counter")) || 0;
 
-// Save user data to localStorage
+// Save user data to localStorage (with form validation)
 function saveUserData() {
+
+    if (!Name.value) {
+        alert("Name is required");
+        return;
+    }
+
+    if (!roll.value) {
+        alert("Roll Number is required");
+        return;
+    }
+
+    if (!address.value) {
+        alert("Address is required");
+        return;
+    }
+
+    alert("Form submitted successfully!");
+
     const user_object = {
         data_name: Name.value,
         data_roll: Number(roll.value),
         data_address: address.value,
     };
-    
+
     i++;
     localStorage.setItem("counter", i);
     localStorage.setItem(`user${i}`, JSON.stringify(user_object));
-    
+
     UpdateTranscript(user_object);
-    
+
     // Clear form
-    Name.value = roll.value = address.value = "";
+    Name.value = "";
+    roll.value = "";
+    address.value = "";
 }
 
 // Display all users from localStorage
 function displayAllUsers() {
     show_data.innerHTML = "";
     const totalUsers = parseInt(localStorage.getItem("counter")) || 0;
-    
+
     for (let j = 1; j <= totalUsers; j++) {
         const user_retrieve = JSON.parse(localStorage.getItem(`user${j}`));
         if (user_retrieve) ShowOutput(user_retrieve, j);
     }
 }
 
-// Update transcript section with latest user
+// Update transcript section
 function UpdateTranscript(obj) {
     transcript.innerHTML = `
         <h2>Transcript</h2>
@@ -50,7 +70,7 @@ function UpdateTranscript(obj) {
     `;
 }
 
-// Append user data to output section
+// Append user data
 function ShowOutput(obj, j) {
     show_data.innerHTML += `
         <div class="border">
@@ -60,7 +80,7 @@ function ShowOutput(obj, j) {
     `;
 }
 
-// Reusable function to generate description list HTML
+// Reusable description list
 function description_list(obj) {
     return `
         <dl class="details">
@@ -75,5 +95,5 @@ function description_list(obj) {
 }
 
 // Event Listeners
-btn_submit?.addEventListener("click", saveUserData);
-btn_output?.addEventListener("click", displayAllUsers);
+btn_submit.addEventListener("click", saveUserData);
+btn_output.addEventListener("click", displayAllUsers);
